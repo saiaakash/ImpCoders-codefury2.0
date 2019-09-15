416a5ffff1d04fbb7fa2c6053336c1016e692f07
saiaakash <p.saiaakash518@gmail.com>
2019-09-15 13:31:58 +0530

    card patent

--- src/components/patentItem.js
+++ src/components/patentItem.js
@@ -14,11 +14,10 @@ render() {
     <Card.Title>Patent Title</Card.Title>
     <Card.Subtitle className="mb-2 text-muted">Patent Description</Card.Subtitle>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+      Thermo nuclear weapon industry component
     </Card.Text>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
+    <Card.Link href="#">Read more</Card.Link>
+    
   </Card.Body>
 </Card>
         </div>
