f75cd465c45dcc09e111b9ba39ae676308f17b3d
saiaakash <p.saiaakash518@gmail.com>
2019-09-15 12:27:32 +0530

    added patent item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/login"
+import PatentItem from "./components/patentItem"
 import { Button } from "react-bootstrap";
 import firebase from "./firebase"
 class App extends React.Component{
@@ -15,8 +16,9 @@ class App extends React.Component{
 render() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 style={{textAlign:"center"}}>Hi Patent</h1>
       <Login/>
+      <PatentItem/>
     </div>  
   )
 }
---
+++ src/components/landing.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component{
+
+  constructor(){
+    super()
+    this.state={
+      textToDisplay:""
+    }
+  }
+
+render() {
+  return (
+    <div>
+      
+    </div>  
+  )
+}
+}
+
+export default App;
